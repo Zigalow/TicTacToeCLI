0bd06b638ff3b267af9995ba39fb61237244c2cb
Zigalow <s224302@dtu.dk>
2024-07-02 23:29:46 +0200

    Done with XML doc of GameMode enum

--- TicTacToeCLI/Model/GameMode.cs
+++ TicTacToeCLI/Model/GameMode.cs
@@ -1,8 +1,17 @@
 namespace TicTacToeCLI.Model;
 
+/// <summary>
+/// Mode of the game being played
+/// </summary>
 public enum GameMode
 {
-    PlayerVersusCPU,
+    /// <summary>
+    /// Game mode of player playing against the CPU 
+    /// </summary>
+    PlayerVersusCpu,
+    /// <summary>
+    /// Game mode of player playing against another local player
+    /// </summary>
     PlayerVersusPlayer,
     
 }
