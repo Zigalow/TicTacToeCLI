9a89d2585c6dbfaa6210f9bd03ebee45c28fa49e
Zigalow <s224302@dtu.dk>
2024-07-04 23:42:20 +0200

    Updated xml doc for IntegerPair class

--- TicTacToeCLI/Model/IntegerPair.cs
+++ TicTacToeCLI/Model/IntegerPair.cs
@@ -1,24 +1,35 @@
 namespace TicTacToeCLI.Model;
 
 /// <summary>
-/// Pair of integers - corresponds to the grid. Integer pair of [0,0] would be upper left
+/// Pair of integers - corresponds to the grid. 
 /// </summary>
+/// <example>
+/// Integer pair of [0,0] would be upper left
+/// <code>
+/// IntegerPair pair = new IntegerPair(0,0);
+/// </code>
+/// </example>
+/// 
 public class IntegerPair(int first, int second)
 {
-    // private readonly int _first;
-    // private readonly int _second;
-
     /// <summary>
-    /// First value of integer pair
+    /// First value of <see cref="IntegerPair"/>
     /// </summary>
     public int First { get; } = first;
 
     /// <summary>
-    /// Second value of integer pair
+    /// Second value of <see cref="IntegerPair"/>
     /// </summary>
     public int Second { get; } = second;
 
-    /// <inheritdoc />
+    /// <summary>
+    /// Returns a string that represents the <see cref="IntegerPair"/>.
+    /// </summary>
+    /// <returns>Returns the <see cref="IntegerPair"/> from the user's point of view. </returns>
+    ///<example>
+    /// An <see cref="IntegerPair"/> of [0,0] would return the string; [1,1], which is the upper left corner of the grid
+    /// </example>
+    /// 
     public override string ToString()
     {
         return $"{First + 1}.{Second + 1}";
