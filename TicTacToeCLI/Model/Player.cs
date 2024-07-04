67099a648ad949de20c9fbe1342fd5fb443a4e89
Zigalow <s224302@dtu.dk>
2024-07-04 23:59:49 +0200

    Done with xml doc and refactoring of player

--- TicTacToeCLI/Model/Player.cs
+++ TicTacToeCLI/Model/Player.cs
@@ -1,13 +1,39 @@
-using TicTacToeCLI.Model;
+namespace TicTacToeCLI.Model;
 
-namespace TicTacToeCLI.Models;
-
+/// <summary>
+/// This class represents a player in a game.
+/// It contains a list of <see cref="SymbolPositions"/>, methods for retrieving the list and adding an <see cref="IntegerPair"/> to the list,
+/// as well as a <see cref="Name"/> and <see cref="Symbol"/> property.
+/// </summary>
+/// <remarks>This class should be inherited by a computer player.</remarks>
 public class Player
 {
-    private static int _numberOfPlayers;
-    public List<IntegerPair> PlacedCurrently { get; }
+    /// <summary>
+    /// Represents the current positions of a player's placements of symbols in the game.
+    /// </summary>
+    private readonly List<IntegerPair> _symbolPositions;
 
-    protected int NumberOfPlayers
+    /// <summary>
+    /// Readonly list of the player's symbol positions in the game/>
+    /// </summary>
+    public IReadOnlyList<IntegerPair> SymbolPositions => _symbolPositions.AsReadOnly();
+
+    private static int _numberOfPlayers;
+
+    /// <summary>
+    /// Adds an <see cref="IntegerPair"/> to the list of <see cref="SymbolPositions"/>, representing that a symbol has been placed by a player
+    /// </summary>
+    /// <param name="pair">the integer pair being added to the SymbolPositions list</param>
+    public void AddSymbolPosition(IntegerPair pair)
+    {
+        _symbolPositions.Add(pair);
+    }
+
+    /// <summary>
+    /// Property that returns the number of players in the game. It's designed to not be more than two players.
+    /// </summary>
+
+    private static int NumberOfPlayers
     {
         get
         {
@@ -16,31 +42,55 @@ public class Player
                 _numberOfPlayers = 0;
             }
 
-            return _numberOfPlayers;
+            return ++_numberOfPlayers;
         }
-        init => _numberOfPlayers = value;
     }
 
+    /// <summary>
+    /// The chosen symbol of the player
+    /// </summary>
     public char Symbol { get; }
-    protected string Name { get; init; }
 
-    public Player(char symbol)
+    /// <summary>
+    /// The chosen name of the player
+    /// </summary>
+    private string Name { get; }
+
+    /// <summary>
+    /// Protected constructor for <see cref="Player"/> class.
+    /// Currently intended to give generic name to local players, where null is passed in as name in the chained constructor
+    /// </summary>
+    /// <param name="name">Displayed name of player</param>
+    /// <param name="symbol">Displayed symbol of player</param>
+    protected Player(string? name, char symbol)
     {
         Symbol = symbol;
-        {
-            Name = $"Player {++NumberOfPlayers}";
-        }
-        PlacedCurrently = new List<IntegerPair>();
+        Name = string.IsNullOrEmpty(name) ? $"Player {NumberOfPlayers}" : name;
+        _symbolPositions = new List<IntegerPair>();
     }
 
+    /// <summary>
+    /// Constructor for <see cref="Player"/> class. Chained to the protected constructor, where null is passed in as name, to get generic name for the player
+    /// </summary>
+    /// <param name="symbol">Displayed symbol of player</param>
+    public Player(char symbol) : this(null, symbol)
+    {
+    }
+
+    /// <summary>
+    /// Returns a string that represents the <see cref="Name"/> and <see cref="Symbol"/> of the player.
+    /// </summary>
+    /// <returns>a string that represents the <see cref="Name"/> and <see cref="Symbol"/> of the player</returns>
     public override string ToString()
     {
-        return string.Format(Name + " with symbol of '" + Symbol + "'");
+        return $"{Name} with symbol of '{Symbol}'";
     }
 
-    public virtual void ResetData()
+    /// <summary>
+    /// Clears the data of the player. This includes clearing the list of <see cref="SymbolPositions"/>
+    /// </summary>
+    public virtual void ClearData()
     {
-        PlacedCurrently.Clear();
+        _symbolPositions.Clear();
     }
-    
 }
