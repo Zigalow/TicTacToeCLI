fca49e76b59f9f5723dd94eb73e752f2aeee0e17
Zigalow <s224302@dtu.dk>
2024-07-14 18:17:49 +0200

    Prettified xml doc

--- TicTacToeCLI/Model/IntegerPair.cs
+++ TicTacToeCLI/Model/IntegerPair.cs
@@ -1,35 +1,54 @@
 namespace TicTacToeCLI.Model;
 
+
 /// <summary>
-/// Pair of integers - corresponds to the grid. 
+/// Represents a pair of integers corresponding to coordinates on a grid.
 /// </summary>
+/// <remarks>
+/// The coordinates are zero-based internally, but displayed as one-based to the user.
+/// </remarks>
 /// <example>
-/// Integer pair of [0,0] would be upper left
+/// Creating a pair representing the upper-left corner of the grid:
 /// <code>
-/// IntegerPair pair = new IntegerPair(0,0);
+/// IntegerPair pair = new IntegerPair(0, 0);
 /// </code>
 /// </example>
-/// 
-public class IntegerPair(int first, int second)
+public class IntegerPair
 {
     /// <summary>
-    /// First value of <see cref="IntegerPair"/>
+    /// Initializes a new instance of the <see cref="IntegerPair"/> class.
     /// </summary>
-    public int First { get; } = first;
+    /// <param name="first">The first coordinate (column) of the pair.</param>
+    /// <param name="second">The second coordinate (row) of the pair.</param>
+    public IntegerPair(int first, int second)
+    {
+        First = first;
+        Second = second;
+    }
 
     /// <summary>
-    /// Second value of <see cref="IntegerPair"/>
+    /// Gets the first value (column) of the <see cref="IntegerPair"/>.
     /// </summary>
-    public int Second { get; } = second;
+    /// <remarks>This value is zero-based internally.</remarks>
+    public int First { get; }
+
+    /// <summary>
+    /// Gets the first value (row) of the <see cref="IntegerPair"/>.
+    /// </summary>
+    /// <remarks>This value is zero-based internally.</remarks>
+    public int Second { get; }
 
     /// <summary>
     /// Returns a string that represents the <see cref="IntegerPair"/>.
     /// </summary>
-    /// <returns>Returns the <see cref="IntegerPair"/> from the user's point of view. </returns>
-    ///<example>
-    /// An <see cref="IntegerPair"/> of [0,0] would return the string; [1,1], which is the upper left corner of the grid
+    /// <returns>A string representation of the <see cref="IntegerPair"/> from the user's point of view.</returns>
+    /// <remarks>
+    /// The returned string uses 1-based indexing for user-friendly display.
+    /// </remarks>
+    /// <example>
+    /// An <see cref="IntegerPair"/> of [0,0] would return the string "1.1", 
+    /// which represents the upper-left corner of the grid from the user's perspective.
     /// </example>
-    /// 
     public override string ToString()
     {
         return $"{First + 1}.{Second + 1}";
