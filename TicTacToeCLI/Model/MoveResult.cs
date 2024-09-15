30cee2c3d4ecf6916e3cd7db83a91fbc6b0f8e1b
Zigalow <s224302@dtu.dk>
2024-09-15 14:50:28 +0200

    Added MoveResult class that represents the result of parsing and validating a player's move input, as well as MoveStatus that represents the various statuses that can result from a player's move attempt.

---
+++ TicTacToeCLI/Model/MoveResult.cs
@@ -0,0 +1,68 @@
+namespace TicTacToeCLI.Model;
+
+/// <summary>
+/// Represents the result of parsing and validating a player's move input.
+/// </summary>
+public class MoveResult
+{
+    /// <summary>
+    /// Gets the status of the attempted move.
+    /// </summary>
+    public MoveStatus Status { get; }
+
+    /// <summary>
+    /// Gets the parsed move as an IntegerPair, if available.
+    /// </summary>
+    /// <remarks>
+    /// This will be null for move statuses that don't represent a valid move,
+    /// such as UnexpectedInput, DisplayHelp, or InvalidFormat.
+    /// </remarks>
+    public IntegerPair? Move { get; }
+
+    /// <summary>
+    /// Initializes a new instance of the MoveResult class.
+    /// </summary>
+    /// <param name="status">The status of the attempted move.</param>
+    /// <param name="move">The parsed move as an IntegerPair, if available.</param>
+    public MoveResult(MoveStatus status, IntegerPair? move = null)
+    {
+        Status = status;
+        Move = move;
+    }
+}
+
+/// <summary>
+/// Represents the various statuses that can result from a player's move attempt.
+/// </summary>
+public enum MoveStatus
+{
+    /// <summary>
+    /// Indicates that the input was unexpected or could not be interpreted.
+    /// </summary>
+    UnexpectedInput,
+
+    /// <summary>
+    /// Indicates that the player has requested to display control information.
+    /// </summary>
+    DisplayControls,
+
+    /// <summary>
+    /// Indicates that the input was in an incorrect format and could not be parsed as a move.
+    /// </summary>
+    InvalidFormat,
+
+    /// <summary>
+    /// Indicates that the parsed move is outside the boundaries of the game grid.
+    /// </summary>
+    OutOfBounds,
+
+    /// <summary>
+    /// Indicates that the parsed move refers to a space that is already occupied.
+    /// </summary>
+    SpaceOccupied,
+
+    /// <summary>
+    /// Indicates that the parsed move is valid and can be played.
+    /// </summary>
+    Valid
+}
